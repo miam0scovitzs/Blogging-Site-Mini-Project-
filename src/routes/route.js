3774e5d0f4acd8304ea17b65d984b67422f5a0ea
Dibakar <Dibakar Dey>
2022-05-01 23:25:40 +0530

    project1 file

--- src/routes/route.js
+++ src/routes/route.js
@@ -15,7 +15,7 @@ router.post("/AuthorLogin", AuthorController.Authorlogin)
 
 const BlogController = require("../Controller/BlogController")
 const MiddleWare = require("../MiddleWare/auth")
-router.post("/CreateBlog", MiddleWare.authenticate, BlogController.CreateBlog)
+router.post("/CreateBlog", BlogController.CreateBlog)
 router.get("/getBlog", MiddleWare.authenticate, BlogController.getBlog)
 router.put("/Updateblogs/:blogId", MiddleWare.authenticate, MiddleWare.authorise, BlogController.UpdateBlog)
 router.delete("/blogs/:blogId", MiddleWare.authenticate, MiddleWare.authorise, BlogController.DeleteBlogID)
